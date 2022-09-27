274e25924c88a9435ea9dfe5dad736249d02ef6e
ayan <ayanalam@cedcoss.com>
2022-09-27 13:13:31 +0530

    Array exercise -2

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
 // -----------------Array exercise -1 -----------
 const cars = ["saab", "volvo", "BMW"];
 console.log(cars[1]);
+
+// ----------------Array exercise - 2 -----------
+const fruits = ["banana", "mango", "apple"];
+console.log(fruits.pop());
+document.getElementById('demo').innerHTML = fruits.pop();
